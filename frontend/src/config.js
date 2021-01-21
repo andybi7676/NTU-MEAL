cdead95b2fef129a35454f18455314620b7c17f7
andybi7676 <b07502072@ntu.edu.tw>
2021-01-21 18:47:20 +0800

    sss

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const BACKEND="http://localhost/:3001"
+export const BACKEND="http://localhost/"
 // export const BACKEND="https://tanet30-back.tp1rc.edu.tw";
 export const loginURL="http://localhost:3000/login";
 // export const loginURL="https://tanet30.tp1rc.edu.tw/login";
