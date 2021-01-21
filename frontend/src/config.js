d15227f92c9de60efd0b7eb171fbf92febc94ca1
andybi7676 <b07502072@ntu.edu.tw>
2021-01-21 18:34:42 +0800

    no cors

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
-export const BACKEND="http://13.76.199.115/:3001"
+export const BACKEND="http://localhost/:3001"
 // export const BACKEND="https://tanet30-back.tp1rc.edu.tw";
-export const loginURL="http://13.76.199.115:3000/login";
+export const loginURL="http://localhost:3000/login";
 // export const loginURL="https://tanet30.tp1rc.edu.tw/login";
-export const FRONTEND="http://13.76.199.115:3000"
+export const FRONTEND="http://localhost:3000"
 // export const FRONTEND="https://tanet30.tp1rc.edu.tw";
 export const localStateName = "NTU-MEAL";
